e66d004b8f35338359c4ce7f9ca888dda03a1e6c
Anka <runner@Mac-1765538428649.local>
2025-12-12 11:59:10 +0000

    Experimental release v1.0.8-alpha1

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SleepCycleSDK",
-            url: "https://github.com/MDLabs/sleepcycle-sdk-swift/releases/download/v1.0.7/SleepCycleSDK-1.0.7.zip",
-            checksum: "0c73fa74c939c66a09a1c4be36cdf55802912ab9c6f21d91fad8b6c4bffb2eb6"
+            url: "https://github.com/MDLabs/sleepcycle-sdk-swift/releases/download/v1.0.8-alpha1/SleepCycleSDK-1.0.8-alpha1.zip",
+            checksum: "325c5ca3b8be2e198627c0064294b3bf5de119239e618ed92563bcf2a817dde1"
         ),
         .binaryTarget(
             name: "SleepCycleObjC",
-            url: "https://github.com/MDLabs/sleepcycle-sdk-swift/releases/download/v1.0.7/SleepCycleObjC-1.0.7.zip",
-            checksum: "b88d8d48fa386851facd447167ede5e12dcf23b3ec6957551d0f7e8a9f0708ff"
+            url: "https://github.com/MDLabs/sleepcycle-sdk-swift/releases/download/v1.0.8-alpha1/SleepCycleObjC-1.0.8-alpha1.zip",
+            checksum: "83272c5e31f61eff6054cd8ae4c3b53cc8e1f8802c0184e7039312735107dc6c"
         )
     ]
 )
